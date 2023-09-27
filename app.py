995740229ba4a4d6349549376f9e5e7a70cb9f9d
sage <hello@sageelliott.com>
2023-09-26 22:10:16 -0700

    fix: basic formatting

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-import openai
-import gradio as gr
 import os
 
+import gradio as gr
+import openai
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Set OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 def summarize_text(user_prompt, system_prompt=None):
-  """Summarize text using OpenAI's GPT-3 API.
+    """Summarize text using OpenAI's GPT-3 API.
 
     Args:
         user_prompt (str): Text to be summarized.
@@ -19,40 +21,39 @@ def summarize_text(user_prompt, system_prompt=None):
         prompt_and_response (dict): Dictionary containing the prompt and response.
     """
 
-  response = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system",
-     "content": system_prompt},
-    {"role": "user",
-     "content": user_prompt}
-    ]
-  )
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
 
-  prompt_and_response = {
-      "prompt": user_prompt,
-      "response": response.choices[0].message.content
-  }
+    prompt_and_response = {
+        "prompt": user_prompt,
+        "response": response.choices[0].message.content,
+    }
 
-  return prompt_and_response
+    return prompt_and_response
 
 
 def llm_response(message):
+    # Create system prompt for LLM behavior
+    instructions = "Summarize this text. Make concise."
 
-      # Create system prompt for LLM behavior
-      instructions = "Summarize this text. Make concise."
+    # Get prompt & response from Summarize function
+    prompt_and_response = summarize_text(message, instructions)
 
-      # Get prompt & response from Summarize function
-      prompt_and_response = summarize_text(message, instructions)
+    response = prompt_and_response["response"]
 
-      response = prompt_and_response["response"]
-
-      return response
+    return response
 
 
 iface = gr.Interface(
     fn=llm_response,
-    inputs = ["text",],
+    inputs=[
+        "text",
+    ],
     outputs="text",
     live=False,
 )
